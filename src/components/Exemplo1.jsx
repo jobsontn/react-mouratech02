76a4b4893e994d72cfa68222190227bacb306289
Jobson Tenório do Nascimento <jobsontenorio@gmail.com>
2023-03-10 20:22:19 -0300

    Rect Router, Eslint, editorConfig

--- src/components/Exemplo1.jsx
+++ src/components/Exemplo1.jsx
@@ -1,37 +1,36 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
 
 class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    const { start } = this.props;
+    this.state = {
+      seconds: start,
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            seconds: this.props.start
-        };
+  contar() {
+    this.setState(
+      (state) => ({ seconds: state.seconds + 1 }),
+    );
+  }
+
+  startStop() {
+    if (this.interval) {
+      clearInterval(this.interval);
+      this.interval = null;
+    } else {
+      this.interval = setInterval(() => this.contar(), 1000);
     }
+  }
 
-    contar() {
-        this.setState(
-            state => ({ seconds: state.seconds + 1 })
-        );
-    }
+  zero() {
+    this.setState(() => ({
+      seconds: 0,
+    }));
+  }
 
-    startStop() {
-        if (this.interval) {
-            clearInterval(this.interval);
-            this.interval = null
-        } else {
-            this.interval = setInterval(() => this.contar(), 1000);
-        }
-    }
-
-    zero() {
-        this.setState(state => ({
-            seconds: 0
-        }));
-    }
-
-    /* componentDidMount() {
+  /* componentDidMount() {
         this.interval = setInterval(() => this.contar(), this.props.ms);
     }
 
@@ -39,24 +38,23 @@ class Timer extends React.Component {
         clearInterval(this.interval);
     } */
 
-    render() {
-        return (
-            <>
-                <div className="bg-black border-black w-48 md:w-96 h-16 m-4 rounded-xl flex flex-col justify-center items-center">
-                    <h1 className="text-5xl text-white cronometro">
-                        {this.state.seconds}
-                    </h1>
-                </div>
-                <div className="flex flex-row justify-center w-48 md:w-96 m-4 ">
-                    <button onClick={() => this.startStop()} className="bg-azulEscuro text-white m-4 px-4 scroll-py-24 w-36">Iniciar/Parar</button>
-                    <button onClick={() => this.zero()} className="bg-red-500 text-white m-4 px-4 scroll-py-24 w-36">Zerar</button>
-                </div>
+  render() {
+    const { seconds } = this.state;
+    return (
+      <>
+        <div className="bg-black border-black w-48 md:w-96 h-16 m-4 rounded-xl flex flex-col justify-center items-center">
+          <h1 className="text-5xl text-white cronometro">
+            {seconds}
+          </h1>
+        </div>
+        <div className="flex flex-row justify-center w-48 md:w-96 m-4 ">
+          <button type="button" onClick={() => this.startStop()} className="bg-azulEscuro text-white m-4 px-4 scroll-py-24 w-36">Iniciar/Parar</button>
+          <button type="button" onClick={() => this.zero()} className="bg-red-500 text-white m-4 px-4 scroll-py-24 w-36">Zerar</button>
+        </div>
 
-            </>
-        )
-            ;
-    }
-
+      </>
+    );
+  }
 }
 
 export default Timer;
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function LoginPage() {
+  return <h1> Página de login </h1>;
+}
+
+export default LoginPage;
